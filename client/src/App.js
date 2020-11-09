3a92faf033f4c51584f4cb52282e869df43584ba
juancaruizc <juancruizcruz@gmail.com>
2020-11-09 16:39:03 -0700

    test

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 import SavedList from './Movies/SavedList';
 
-export default function App () {
+export default function App() {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
   const [movieList, setMovieList] = useState([]);
 
@@ -11,26 +11,34 @@ export default function App () {
     const getMovies = () => {
       axios
         .get('http://localhost:5000/api/movies') // Study this endpoint with Postman
-        .then(response => {
+        .then((response) => {
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movieList' slice of state
         })
-        .catch(error => {
+        .catch((error) => {
           console.error('Server Error', error);
         });
-    }
+    };
     getMovies();
   }, []);
 
-  const addToSavedList = id => {
+  const addToSavedList = (id) => {
     // This is stretch. Prevent the same movie from being "saved" more than once
   };
 
   return (
     <div>
-      <SavedList list={[ /* This is stretch */]} />
+      <SavedList
+        list={
+          [
+            /* This is stretch */
+          ]
+        }
+      />
 
       <div>Replace this Div with your Routes</div>
     </div>
   );
 }
+
+//test
